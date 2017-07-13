4affdf1d669e539a3b65e3660432d53d7ba3b3de
Maor Leger <maor.leger@gmail.com>
2017-07-13 08:55:46 -0600

    Replace notebook look with material theme

--- src/app/taskUpApp.module.ts
+++ src/app/taskUpApp.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { TaskUpAppComponent } from './taskUpApp.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
+
+import {
+  MdListModule,
+  MdCheckboxModule,
+  MdCardModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,11 @@ import { TaskListComponent } from './components/task-list/task-list.component';
     TaskListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdListModule,
+    MdCheckboxModule,
+    MdCardModule
   ],
   providers: [],
   bootstrap: [TaskUpAppComponent]
